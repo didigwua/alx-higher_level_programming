a34b6a965fdaefbc53f1e91367256d302e455351
didigwua <didigwuabuchi@gmail.com>
2022-09-26 13:30:16 -0700

    task 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    return (len(sentence), sentence[0] if len(sentence) > 0 else None)
+def add_tuple(tuple_a=(), tuple_b=()):
+    a, b = len(tuple_a), len(tuple_b)
+    new_tuple = ((tuple_a[0] if a >= 1 else 0) + (tuple_b[0] if b >= 1 else 0),
+            (tuple_a[1] if a >= 2 else 0) + (tuple_b[1] if b >= 2 else 0))
+    return new_tuple
